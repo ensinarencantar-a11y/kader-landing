45d24039170330a592e0ba5b027895e3ed25a6af
ensinarencantar-a11y <ensinarencantar@gmail.com>
2026-05-20 09:07:59 -0300

    git- corrigir espaçamento - hero

--- src/App.tsx
+++ src/App.tsx
@@ -263,7 +263,7 @@ export default function App() {
                 <div className="absolute inset-0 bg-gradient-to-r from-[#F7F6F2]/45 via-transparent to-transparent" />
               </div>
 
-              <div className="relative mt-mt-4 mx-auto z-30 w-[85%] rounded-[30px] border border-white bg-[#071B66] p-6 shadow-2xl lg:absolute lg:bottom-8 lg:right-[-30px] lg:w-[360px]">
+              <div className="absolute bottom-4 left-1/2 z-30 w-[85%] -translate-x-1/2 rounded-[30px] border border-white bg-[#071B66] p-6 shadow-2xl lg:bottom-8 lg:right-[-30px] lg:left-auto lg:w-[360px] lg:translate-x-0">
                 <div className="space-y-4">
                   {[
                     
